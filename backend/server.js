fbb7ca832c7063eb567c974699b7314b93cbc07e
arrow123-web <liscitaku@gmail.com>
2025-12-30 01:38:18 +0100

    new commit

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.get('/products/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
