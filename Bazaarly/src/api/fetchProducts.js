d8a0b1c0828d2246368c9c513217b8f834ef4e3b
arrow123-web <liscitaku@gmail.com>
2025-12-30 01:31:02 +0100

    fix

--- Bazaarly/src/api/fetchProducts.js
+++ Bazaarly/src/api/fetchProducts.js
@@ -1,20 +1,13 @@
+const API_URL = import.meta.env.VITE_API_URL; // <-- use this
+
 export async function fetchProducts() {
-  const res = await fetch("http://localhost:3000/products");
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch product: ${res.status}`);
-  }
-
+  const res = await fetch(`${API_URL}/products`);
+  if (!res.ok) throw new Error(`Failed to fetch products: ${res.status}`);
   return res.json();
 }
 
-
 export async function fetchProduct({ id }) {
-  const res = await fetch(`http://localhost:3000/products/${id}`);
-  
-  if (!res.ok) {
-    throw new Error(`Failed to fetch product: ${res.status}`);
-  }
-
+  const res = await fetch(`${API_URL}/products/${id}`);
+  if (!res.ok) throw new Error(`Failed to fetch product: ${res.status}`);
   return res.json();
 }
